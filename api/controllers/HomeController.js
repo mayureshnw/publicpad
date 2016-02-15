756e26ce534b280faba68821630201aa1cf0de4c
mayuresh <mayuresh@Aasheesh-MacBook-Pro.local>
2016-02-15 16:34:07 +0530

    initalized api for home and notebook

---
+++ api/controllers/HomeController.js
@@ -0,0 +1,11 @@
+/**
+ * HomeController
+ *
+ * @description :: Server-side logic for managing homes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/controllers/NotebookController.js
@@ -0,0 +1,11 @@
+/**
+ * NotebookController
+ *
+ * @description :: Server-side logic for managing notebooks
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
